2ee209adb4bf4859f3c0b29fc0228636e4f2a66a
Radon Rosborough <radon.neon@gmail.com>
2016-07-23 15:41:33 -0600

    Add .lein/profiles.clj
    Also ignore the other stuff in the .lein folder.

---
+++ .lein/profiles.clj
@@ -0,0 +1,12 @@
+{:user {:plugins [[cider/cider-nrepl "0.12.0"]
+
+                  ;; Linters
+                  [jonase/eastwood "0.2.3"]
+                  [lein-ancient "0.6.10"]
+                  [lein-bikeshed "0.3.0"]
+                  [lein-cljfmt "0.5.3"]
+                  [lein-kibit "0.1.2"]
+                  [venantius/yagni "0.1.4"]
+
+                  ;; Documentation generators
+                  [lein-codox "0.9.5"]]}}
