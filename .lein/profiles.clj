cae879389fc148959350c187a65bb62d40e2557d
Radon Rosborough <radon.neon@gmail.com>
2016-07-29 18:30:42 -0600

    Hide the Clojure REPL in the Mac app switcher

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -7,4 +7,8 @@
                   [venantius/yagni "0.1.4"]
 
                   ;; Documentation generators
-                  [lein-codox "0.9.5"]]}}
+                  [lein-codox "0.9.5"]]
+
+        ;; Prevent the Clojure REPL from showing in the Mac app switcher.
+        ;; See http://stackoverflow.com/questions/24619300/hide-clojure-repl-from-command-tab-application-switcher-via-lein-command-line
+        :jvm-opts ["-Dapple.awt.UIElement=true"]}}
