136d05c729fa2ab62c8225db8c99e755e26a1e52
Radon Rosborough <radon.neon@gmail.com>
2016-08-02 19:52:07 -0600

    Setup script: force $(which zsh) to be used in chsh

--- scripts/change_login_shell.sh
+++ scripts/change_login_shell.sh
@@ -3,19 +3,19 @@
 set -e
 
 echo "[setup] Checking the login shell."
-if finger "$USER" | grep 'Shell: /.*/zsh'; then
-    echo "[setup] The login shell appears to already be zsh."
+if finger "$USER" | grep "Shell: $(which zsh)"; then
+    echo "[setup] The login shell appears to already be $(which zsh)."
 else
     if ! hash finger 2>/dev/null; then
         echo "[setup] The 'finger' utility does not appear to be available. Can't determine the login shell."
     fi
-    echo '[setup] The login shell appears to be something other than zsh.'
+    echo "[setup] The login shell appears to be something other than $(which zsh)."
     if ! fgrep -q "$(which zsh)" /etc/shells; then
-        echo '[setup] It looks like zsh is not designated as an acceptable login shell.'
+        echo "[setup] It looks like $(which zsh) is not designated as an acceptable login shell."
         echo "[setup] Adding $(which zsh) to /etc/shells."
         sudo -s 'echo "$(which zsh)" >> /etc/shells'
     fi
-    echo '[setup] Changing the login shell to zsh.'
+    echo "[setup] Changing the login shell to $(which zsh)."
     until chsh -s "$(which zsh)"; do
         echo "[setup] Encountered an error. Trying again in two seconds."
         sleep 2
