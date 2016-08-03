4ef1cfc6c8da11b9941536aac7ae126e5365b95c
Radon Rosborough <radon.neon@gmail.com>
2016-08-03 08:10:11 -0600

    Only symlink init.el, not whole .emacs.d folder

--- scripts/create_emacs_symlinks.sh
+++ scripts/create_emacs_symlinks.sh
@@ -18,16 +18,17 @@ else
     echo "[setup] Looks like you don't have one."
 fi
 
-echo '[setup] Checking for a ~/.emacs.d.'
-if [[ ~/.emacs.d -ef ../.emacs.d ]]; then
-    echo '[setup] It appears that ~/.emacs.d is already correctly symlinked.'
+echo '[setup] Checking for a ~/.emacs.d/init.el.'
+if [[ ~/.emacs.d/init.el -ef ../init.el ]]; then
+    echo '[setup] It appears that ~/.emacs.d/init.el is already correctly symlinked.'
 else
-    if [[ -e ~/.emacs.d || -L ~/.emacs.d ]]; then
+    if [[ -e ~/.emacs.d/init.el || -L ~/.emacs.d/init.el ]]; then
         echo "[setup] Found one, moving it to originals/$UUID."
-        mv ~/.emacs.d originals/$UUID/.emacs.d
+        mv ~/.emacs.d/init.el originals/$UUID/init.el
     else
         echo "[setup] Looks like you don't have one."
     fi
-    echo '[setup] Creating symlink for .emacs.d.'
-    ln -s "$(cd .. && pwd)/.emacs.d" ~/.emacs.d
+    echo '[setup] Creating symlink for init.el.'
+    mkdir ~/.emacs.d 2>/dev/null || true
+    ln -s "$(cd .. && pwd)/init.el" ~/.emacs.d/init.el
 fi
