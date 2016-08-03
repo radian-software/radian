841651d6399503689b600c2a73357e2745b29754
Radon Rosborough <radon.neon@gmail.com>
2016-08-02 18:14:27 -0600

    Wait before asking if Xcode CL tools are installed

--- scripts/install_xcode_cl_tools.sh
+++ scripts/install_xcode_cl_tools.sh
@@ -7,6 +7,7 @@ echo '[setup] Checking if the Xcode Command Line Tools are installed.'
 while xcode-select --install; do
     echo '[setup] The Xcode Command Line Tools do not appear to be installed.'
     echo '[setup] Please use the dialog window to install the Xcode Command Line Tools.'
+    sleep 3
     read -p '[setup] Please press RET when you have finished installing the Xcode Command Line Tools.'
 done
 
