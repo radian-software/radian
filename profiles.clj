437fb6ff85e5ffde71fb8db7d93c1059fc069e26
Radon Rosborough <radon.neon@gmail.com>
2016-08-07 10:59:33 -0600

    Ensure recent version of tools.nrepl for CIDER

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,12 @@
 {;;; Modular profiles
 
- :cider {:dependencies [;; CIDER requires Clojure 1.7.0+ to work
-                        [org.clojure/clojure "1.8.0"]]
+ :cider {:dependencies [;; CIDER 0.13.0 requires Clojure 1.7.0+ and
+                        ;; tools.nrepl 0.2.12+ to work. These are used
+                        ;; by default, but projects might want to
+                        ;; override them. If we're using CIDER though,
+                        ;; we don't want to let them do that.
+                        [org.clojure/clojure "1.8.0"]
+                        [org.clojure/tools.nrepl "0.2.12"]]
 
          :plugins [;; REPL-side support for CIDER and other editor tools
                    [cider/cider-nrepl "0.13.0"]]}
