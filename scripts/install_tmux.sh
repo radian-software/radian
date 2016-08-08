f4c3d6785bbd073ceeb2230da8574c7e7efc346f
Radon Rosborough <radon.neon@gmail.com>
2016-08-08 07:47:31 -0600

    Ensure at least version 2.2 of tmux

--- scripts/install_tmux.sh
+++ scripts/install_tmux.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
-echo '[setup] Checking to see if tmux is installed.'
-if hash tmux; then
-    echo '[setup] It appears that tmux is already installed.'
+echo '[setup] Checking to see if tmux 2.2 or newer is installed.'
+
+tmux_version_regex='tmux (2\.([2-9]|[1-9][0-9]+)|[3-9]|[1-9][0-9]+)'
+tmux_version_regex_brew='tmux.* (5\.([2-9]|[1-9][0-9]+)|[6-9]|[1-9][0-9]+)'
+
+if tmux -V | egrep "$tmux_version_regex"; then
+    echo '[setup] It looks like an appropriate version of tmux is already installed.'
 else
-    if [[ $(brew list --versions tmux) ]]; then
-        echo '[setup] It appears that tmux has already been installed using Homebrew, but it is not symlinked correctly.'
+    if brew list --versions tmux | egrep "$tmux_version_regex_brew"; then
+        echo '[setup] It looks like an appropriate version of tmux has already been installed using Homebrew, but it is not symlinked correctly.'
         echo '[setup] Removing old symlinks using Homebrew.'
         brew unlink tmux
         echo '[setup] Recreating symlinks using Homebrew.'
         brew link tmux
     else
-        echo '[setup] It appears that tmux is not yet installed.'
+        echo '[setup] It looks like an appropriate version of tmux is not installed.'
+        if [[ $(brew list --versions tmux) ]]; then
+            echo '[setup] Uninstalling the old version of tmux using Homebrew.'
+            echo -n '[setup] Is this OK? (y/n) '
+            read answer
+            if ! (echo "$answer" | grep -qi "^y"); then
+                echo '[setup] Aborting.'
+                exit 1
+            fi
+            brew uninstall tmux
+        fi
         echo '[setup] Installing tmux using Homebrew.'
         brew install tmux
     fi
-    if ! hash tmux; then
-        echo '[setup] Fatal error: tmux should be installed by this point.'
+    if ! (tmux -V | egrep "$tmux_version_regex"); then
+        echo '[setup] Fatal error: an appropriate version of tmux should be installed by this point.'
         exit 1
     fi
 fi
