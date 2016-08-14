40582b222bd7a8fcd082b4bfd073c8f93d73eb12
Radon Rosborough <radon.neon@gmail.com>
2016-08-14 17:34:48 -0600

    Make setup script modular
    Resolves #71.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,9 +16,13 @@ if [[ $0 != $BASH_SOURCE ]]; then
     return 1 2>/dev/null || exit 1
 fi
 
+### Compute features ###
+
+source compute-features.sh
+
 ### Setup ###
 
-echo '[setup] Setting up raxod502/dotfiles.'
+echo "[setup] Setting up raxod502/dotfiles. Prepare to be amazed."
 
 trap 'echo && echo "[setup] It looks like an error occurred. Please try to fix it, and then run this script again."' EXIT
 
@@ -32,49 +36,70 @@ echo "[setup] The UUID for this session is $uuid."
 ### Bootstrapping ###
 
 ./ensure-xcode-cl-tools-installed.sh
-./ensure-installed.sh brew --version Homebrew any-version ./install-homebrew.sh
-./ensure-installed.sh wget
+
+if feature brew; then
+    ./ensure-installed.sh brew --version Homebrew any-version ./install-homebrew.sh
+fi
+
+if feature wget; then
+    ./ensure-installed.sh wget
+fi
 
 ### Git ###
 
-./ensure-symlinked.sh ~/.gitconfig ../.gitconfig
-./ensure-gitconfig-local-exists.sh
+if feature git; then
+    ./ensure-symlinked.sh ~/.gitconfig ../.gitconfig
+    ./ensure-gitconfig-local-exists.sh
+fi
 
 ### Zsh ###
 
-./ensure-installed.sh zsh --version zsh 5.2
-./ensure-login-shell-set.sh
-./ensure-antigen-installed.sh
-./ensure-installed.sh autojump
-./ensure-symlinked.sh ~/.zshrc ../.zshrc
+if feature zsh; then
+    ./ensure-installed.sh zsh --version zsh 5.2
+    ./ensure-login-shell-set.sh
+    ./ensure-antigen-installed.sh
+    ./ensure-installed.sh autojump
+    ./ensure-symlinked.sh ~/.zshrc ../.zshrc
+fi
 
 ### Tmux ###
 
-./ensure-installed.sh tmux -V tmux 2.2
-./ensure-symlinked.sh ~/.tmux.conf ../.tmux.conf
+if feature tmux; then
+    ./ensure-installed.sh tmux -V tmux 2.2
+    ./ensure-symlinked.sh ~/.tmux.conf ../.tmux.conf
+fi
 
 ### Leiningen ###
 
-./ensure-installed.sh javac -version javac 1.6 ./install-jdk.sh
-./ensure-installed.sh lein --version Leiningen 2.6.1 brew leiningen
-./ensure-symlinked.sh ~/.lein/profiles.clj ../profiles.clj
+if feature leiningen; then
+    ./ensure-installed.sh javac -version javac 1.6 ./install-jdk.sh
+    ./ensure-installed.sh lein --version Leiningen 2.6.1 brew leiningen
+    ./ensure-symlinked.sh ~/.lein/profiles.clj ../profiles.clj
+fi
 
 ### Emacs ###
 
-./ensure-installed.sh emacs --version "GNU Emacs" 24.5.1 ./install-emacs.sh
-if [[ /usr/local/bin/emacs -ef emacs ]]; then
-    ./ensure-symlinked.sh /usr/local/bin/emacsw emacsw
-else
-    ./ensure-symlinked.sh /usr/local/bin/emacsw "$(which emacs)"
+if feature emacs; then
+    ./ensure-installed.sh emacs --version "GNU Emacs" 24.5.1 ./install-emacs.sh
+    if [[ /usr/local/bin/emacs -ef emacs ]]; then
+        ./ensure-symlinked.sh /usr/local/bin/emacsw emacsw
+    else
+        ./ensure-symlinked.sh /usr/local/bin/emacsw "$(which emacs)"
+    fi
+    ./ensure-symlinked.sh ~/.emacs
+    ./ensure-symlinked.sh ~/.emacs.el
+    ./ensure-symlinked.sh ~/.emacs.d/init.el ../init.el
 fi
-./ensure-symlinked.sh ~/.emacs
-./ensure-symlinked.sh ~/.emacs.el
-./ensure-symlinked.sh ~/.emacs.d/init.el ../init.el
 
 ### Utilities ###
 
-./ensure-installed.sh tree
-./ensure-installed.sh tmuxinator version tmuxinator 0.8.1 gem
+if feature tree; then
+    ./ensure-installed.sh tree
+fi
+
+if feature tmuxinator; then
+    ./ensure-installed.sh tmuxinator version tmuxinator 0.8.1 gem
+fi
 
 ### Cleanup ###
 
@@ -87,9 +112,13 @@ echo
 echo "[setup] We're all done. Enjoy!"
 echo "[setup] Note that programs such as Zsh, Leiningen, and Emacs may still have to download dependencies."
 echo "[setup] If the dotfiles repository is not in the correct place, simply move it and run this script again. Your symlinks will be updated automatically."
-echo "[setup] Starting a new shell session in $(pwd)."
-echo "[setup] Please be aware that the new shell session will be nested inside your original shell session."
-echo "[setup] Any aliases or environment variables will not be cleared."
+if feature zsh; then
+    echo "[setup] Starting a new shell session in $(pwd)."
+    echo "[setup] Please be aware that the new shell session will be nested inside your original shell session."
+    echo "[setup] Any aliases or environment variables will not be cleared."
+fi
 read -p "[setup] Press RET to continue."
 
-exec zsh -l
+if feature zsh; then
+    exec zsh -l
+fi
