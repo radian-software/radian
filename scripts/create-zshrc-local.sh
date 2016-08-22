1f5896e7dc0047d5144d63d8b971201bee833be7
Radon Rosborough <radon.neon@gmail.com>
2016-08-21 19:22:01 -0600

    Preserve $EDITOR in setup script

--- scripts/create-zshrc-local.sh
+++ scripts/create-zshrc-local.sh
@@ -9,6 +9,17 @@ contents=$(cat <<'EOF'
 # override things or add your own customizations.
 EOF
         )
-echo "$contents" > ../../dotfiles-local/.zshrc.local
+contents="$contents"$'\n'
+
+if [[ $EDITOR ]]; then
+    echo "[create-zshrc-local] You currently have \$EDITOR set to $EDITOR."
+    echo -n "[create-zshrc-local] Would you like to carry this over to .zshrc.local? (y/n) "
+    read answer
+    if (echo "$answer" | grep -qi "^y"); then
+        contents="$contents"$'\n'"export EDITOR='$EDITOR'"$'\n'
+    fi
+fi
+
+echo -n "$contents" > ../../dotfiles-local/.zshrc.local
 echo "[create-zshrc-local] Wrote the following to dotfiles-local/.zshrc.local:"
 cat ../../dotfiles-local/.zshrc.local
