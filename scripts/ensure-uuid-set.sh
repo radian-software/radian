e56b0f83138066fd79904849c7be210b27f39240
Radon Rosborough <radon.neon@gmail.com>
2016-08-22 20:15:14 -0600

    Fix typo in ensure-uuid-set.sh message

--- scripts/ensure-uuid-set.sh
+++ scripts/ensure-uuid-set.sh
@@ -2,6 +2,6 @@
 
 if [[ -z $uuid ]]; then
     script_name="${0##*/}"
-    echo "[#{script_name%.sh}] Fatal error: \$uuid not set."
+    echo "[${script_name%.sh}] Fatal error: \$uuid not set."
     exit 1
 fi
