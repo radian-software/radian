da6fc67c77e1b3eef88fbadfcad61c6736737710
Radon Rosborough <radon.neon@gmail.com>
2016-11-20 13:04:47 -0800

    Add ElDoc integration for Irony

--- init.el
+++ init.el
@@ -1434,6 +1434,17 @@ following :dependencies to be enabled."
             (lambda ()
               (add-to-list 'company-backends 'company-irony))))
 
+;; ElDoc integration for Irony.
+(use-package irony-eldoc
+  :dependencies (irony)
+  :quelpa (irony-eldoc
+           :fetcher github
+           :repo "josteink/irony-eldoc")
+  :init
+
+  ;; Enable irony-eldoc.
+  (add-hook 'irony-mode-hook 'irony-eldoc))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Packages: Markdown
 
