d7aad7e9de991355bf2bbc1a6f9f305e4499e47f
Radon Rosborough <radon.neon@gmail.com>
2016-12-13 09:15:53 -0800

    (wip) remove zsh files

--- zsh/completion.zsh
+++
@@ -1,6 +0,0 @@
-# FIXME
-
-# zstyle ':completion:*' matcher 'm:{a-z\-}={A-Z\_}'
-# zstyle ':completion:*:files' matcher-list 'r:|?=** m:{a-z\-}={A-Z\_}'
-# zstyle ':completion:*:directories' matcher-list 'r:|?=** m:{a-z\-}={A-Z\_}'
-# zstyle ':completion:*:local-directories' matcher-list 'r:|?=** m:{a-z\-}={A-Z\_}'
--- zsh/fasd.zsh
+++
@@ -1,1 +0,0 @@
-export _FASD_NOCASE=1
