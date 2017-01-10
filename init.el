000b2191842a5178ba680b2eed856ec9488b4bf4
Radon Rosborough <radon.neon@gmail.com>
2017-01-10 14:06:48 -0700

    Enable delete-selection-mode in Emacs

--- init.el
+++ init.el
@@ -929,6 +929,22 @@ This filter de-installs itself after this call."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Killing and yanking
 
+;; If you start typing when you have something selected, then the
+;; selection will be deleted. If you press DEL while you have
+;; something selected, it will be deleted rather than
+;; killed. (Otherwise, in both cases the selection is deselected and
+;; the normal function of the key is performed.)
+(delete-selection-mode 1)
+
+;; Make delete-selection-mode work properly with Paredit (so that, for
+;; example, pressing DEL while there is a selection will actually
+;; cause the region to be deleted, as expected). See the commentary in
+;; delsel.el for details about what is going on here.
+(with-eval-after-load 'paredit
+  (put 'paredit-forward-delete 'delete-selection 'supersede)
+  (put 'paredit-backward-delete 'delete-selection 'supersede)
+  (put 'paredit-newline 'delete-selection t))
+
 ;; Eliminate duplicates in the kill ring. That is, if you kill the
 ;; same thing twice, you won't have to use M-y twice to get past it to
 ;; older entries in the kill ring.
