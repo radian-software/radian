2b15faec60700f818dd8b4380a895e5e8cfd0c24
Radon Rosborough <radon.neon@gmail.com>
2017-01-19 10:47:07 -0800

    Override keybindings for Org Agenda too

--- init.el
+++ init.el
@@ -842,13 +842,24 @@ This filter de-installs itself after this call."
   (define-key org-mode-map (kbd "S-<right>") nil)
   (define-key org-mode-map (kbd "S-<up>") nil)
   (define-key org-mode-map (kbd "S-<down>") nil)
+  (define-key org-agenda-mode-map (kbd "S-<up>") nil)
+  (define-key org-agenda-mode-map (kbd "S-<down>") nil)
+  (define-key org-agenda-mode-map (kbd "S-<left>") nil)
+  (define-key org-agenda-mode-map (kbd "S-<right>") nil)
 
   ;; Add replacements for the some of keybindings we just removed. It
   ;; looks like Org already binds C-up and C-down separately from M-{
   ;; and M-}, so we can't use those. Users will just have to make do
   ;; with C-c <up> and C-c <down> for now.
+  ;;
+  ;; Now for Org Agenda on the other hand, we could use C-up and
+  ;; C-down because M-{ and M-} are bound to the same commands. But I
+  ;; think it's best to take the same approach as before, for
+  ;; consistency.
   (define-key org-mode-map (kbd "C-<left>") #'org-shiftleft)
-  (define-key org-mode-map (kbd "C-<right>") #'org-shiftright))
+  (define-key org-mode-map (kbd "C-<right>") #'org-shiftright)
+  (define-key org-agenda-mode-map (kbd "C-<left>") #'org-agenda-do-date-earlier)
+  (define-key org-agenda-mode-map (kbd "C-<right>") #'org-agenda-do-date-later))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; ElDoc
