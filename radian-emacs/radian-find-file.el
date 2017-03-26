9c660b2c4b1f3ecf23a70d3df7847fbd4aec4352
Radon Rosborough <radon.neon@gmail.com>
2017-03-26 09:05:41 -0700

    Inhibit superfluous message from save-place-mode

--- radian-emacs/radian-find-file.el
+++ radian-emacs/radian-find-file.el
@@ -303,6 +303,34 @@ This is a function for `after-save-hook'. Remove
 ;; [1]: https://www.emacswiki.org/emacs/SavePlace
 (save-place-mode 1)
 
+;; Inhibit the message that is usually printed when the `saveplace'
+;; file is written.
+(el-patch-defun save-place-alist-to-file ()
+  (let ((file (expand-file-name save-place-file))
+        (coding-system-for-write 'utf-8))
+    (with-current-buffer (get-buffer-create " *Saved Places*")
+      (delete-region (point-min) (point-max))
+      (when save-place-forget-unreadable-files
+        (save-place-forget-unreadable-files))
+      (insert (format ";;; -*- coding: %s -*-\n"
+                      (symbol-name coding-system-for-write)))
+      (let ((print-length nil)
+            (print-level nil))
+        (pp save-place-alist (current-buffer)))
+      (let ((version-control
+             (cond
+              ((null save-place-version-control) nil)
+              ((eq 'never save-place-version-control) 'never)
+              ((eq 'nospecial save-place-version-control) version-control)
+              (t
+               t))))
+        (condition-case nil
+            ;; Don't use write-file; we don't want this buffer to visit it.
+            (write-region (point-min) (point-max) file
+                          (el-patch-add nil 'nomsg))
+          (file-error (message "Saving places: can't write %s" file)))
+        (kill-buffer (current-buffer))))))
+
 ;; This package introduces a concept of "projects" and provides
 ;; commands for quick navigation within and between them.
 (use-package projectile
