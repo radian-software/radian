240793cadfccc28495c552c0f9eac52d2e84943d
Radon Rosborough <radon.neon@gmail.com>
2017-05-02 14:31:13 -0700

    Suppress help window hints
    Closes #169.

--- radian-emacs/radian-help.el
+++ radian-emacs/radian-help.el
@@ -2,6 +2,16 @@
 
 (require 'radian-package)
 
+;; Suppress the "Type q in help window to delete it" hints.
+
+(defalias 'radian--advice-inhibit-help-window-hints #'ignore
+  "Inhibit the \"Type q in help window to delete it\" hints.
+This is an `:override' advice for
+`help-window-display-message'.")
+
+(advice-add #'help-window-display-message :override
+            #'radian--advice-inhibit-help-window-hints)
+
 ;; This package provides several useful features, but the most
 ;; important is `describe-keymap'. Because the package is rather
 ;; outdated, it's not autoloaded. (But `use-package' takes care of
