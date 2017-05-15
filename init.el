16897199dcacf18388fed08cf5a67d0d7c80e394
Radon Rosborough <radon.neon@gmail.com>
2017-05-15 11:37:23 -0700

    Error out properly on Emacs 22

--- init.el
+++ init.el
@@ -23,76 +23,78 @@
         (radian-local . "radian-local.el")
         (nil . "default.el")))
 
-(condition-case-unless-debug error-data
-    (progn
-      ;; Make sure we are running a modern enough Emacs, otherwise
-      ;; abort init.
-      (when (version< emacs-version radian-minimum-emacs-version)
-        (error (concat "Radian Emacs requires at least Emacs %s, "
-                       "but you are running Emacs %s")
-               radian-minimum-emacs-version emacs-version))
+;; Make sure we are running a modern enough Emacs, otherwise abort
+;; init. We have to do this outside the `condition-case-unless-debug'
+;; form, since old Emacsen do not actually have
+;; `condition-case-unless-debug' defined.
+(if (version< emacs-version radian-minimum-emacs-version)
+    (warn (concat "Radian Emacs requires at least Emacs %s, "
+                  "but you are running Emacs %s")
+          radian-minimum-emacs-version emacs-version)
+  ;; We have a modern Emacs, proceed with init.
+  (condition-case-unless-debug error-data
+      (progn
+        ;; Require some libraries that everyone needs, just to be
+        ;; explicit about it.
+        (require 'cl-lib)
+        (require 'subr-x)
 
-      ;; Require some libraries that everyone needs, just to be
-      ;; explicit about it.
-      (require 'cl-lib)
-      (require 'subr-x)
+        ;; Load local customizations.
+        (load radian-local-init-file 'noerror 'nomessage)
 
-      ;; Load local customizations.
-      (load radian-local-init-file 'noerror 'nomessage)
+        ;; Make the Radian libraries available.
+        (add-to-list 'load-path radian-directory)
 
-      ;; Make the Radian libraries available.
-      (add-to-list 'load-path radian-directory)
-
-      ;; Load the Radian libraries.
-      (let ((preloaded-features
-             '(;; no-littering changes lots of paths and needs to be
-               ;; loaded as soon as possible.
-               radian-emacsd))
-            (radian-features (mapcar
-                              (lambda (file)
-                                (intern (string-remove-suffix ".el" file)))
-                              (directory-files
-                               radian-directory nil
-                               "^[a-z-]+\\.el$"
-                               'nosort)))
-            ;; Any packages installed here are official Radian packages.
-            (straight-current-profile 'radian)
-            (init-successful t))
-        ;; First we need to unload all the features, so that the
-        ;; init-file can be reloaded to pick up changes.
-        (dolist (feature radian-features)
-          (setq features (remove feature features)))
-        (dolist (feature preloaded-features)
-          (condition-case-unless-debug error-data
-              (require feature)
-            (error (warn "Could not load `%S': %s" feature
-                         (error-message-string error-data))
-                   (setq init-successful nil))))
-        (dolist (feature radian-features)
-          (unless (member feature preloaded-features)
+        ;; Load the Radian libraries.
+        (let ((preloaded-features
+               '(;; no-littering changes lots of paths and needs to be
+                 ;; loaded as soon as possible.
+                 radian-emacsd))
+              (radian-features (mapcar
+                                (lambda (file)
+                                  (intern (string-remove-suffix ".el" file)))
+                                (directory-files
+                                 radian-directory nil
+                                 "^[a-z-]+\\.el$"
+                                 'nosort)))
+              ;; Any packages installed here are official Radian packages.
+              (straight-current-profile 'radian)
+              (init-successful t))
+          ;; First we need to unload all the features, so that the
+          ;; init-file can be reloaded to pick up changes.
+          (dolist (feature radian-features)
+            (setq features (remove feature features)))
+          (dolist (feature preloaded-features)
             (condition-case-unless-debug error-data
                 (require feature)
               (error (warn "Could not load `%S': %s" feature
                            (error-message-string error-data))
-                     (setq init-successful nil)))))
+                     (setq init-successful nil))))
+          (dolist (feature radian-features)
+            (unless (member feature preloaded-features)
+              (condition-case-unless-debug error-data
+                  (require feature)
+                (error (warn "Could not load `%S': %s" feature
+                             (error-message-string error-data))
+                       (setq init-successful nil)))))
 
-        ;; Run local customizations that are supposed to be run after
-        ;; init. Any packages installed here are user-local
-        ;; packages. (Packages installed interactively do not belong to
-        ;; either `radian' or `radian-local', and should not be written
-        ;; to either lockfile.)
-        (let ((straight-current-profile 'radian-local))
-          (when (fboundp 'radian-after-init)
-            (radian-after-init)))
+          ;; Run local customizations that are supposed to be run after
+          ;; init. Any packages installed here are user-local
+          ;; packages. (Packages installed interactively do not belong to
+          ;; either `radian' or `radian-local', and should not be written
+          ;; to either lockfile.)
+          (let ((straight-current-profile 'radian-local))
+            (when (fboundp 'radian-after-init)
+              (radian-after-init)))
 
-        ;; This helps out the package management system. See the
-        ;; documentation on `straight-declare-init-succeeded'.
-        (when init-successful
-          (straight-declare-init-succeeded))))
+          ;; This helps out the package management system. See the
+          ;; documentation on `straight-declare-init-succeeded'.
+          (when init-successful
+            (straight-declare-init-succeeded))))
 
-  ;; Report errors as warnings.
-  (error (warn "%s" (error-message-string error-data))
-         ;; This helps out the package management system. See the
-         ;; documentation on `straight-declare-init-finished'.
-         (when (fboundp 'straight-declare-init-finished)
-           (straight-declare-init-finished))))
+    ;; Report errors as warnings.
+    (error (warn "%s" (error-message-string error-data))
+           ;; This helps out the package management system. See the
+           ;; documentation on `straight-declare-init-finished'.
+           (when (fboundp 'straight-declare-init-finished)
+             (straight-declare-init-finished)))))
