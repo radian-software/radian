59999332f8efa00c681d7e02376fa42ac7b6aae4
Radon Rosborough <radon.neon@gmail.com>
2017-06-08 15:29:32 -0700

    Add json-mode and requisite gymnastics

--- radian-emacs/radian-langs.el
+++ radian-emacs/radian-langs.el
@@ -174,6 +174,54 @@
   ;; This allows Company to use suggestions from Tern.
   (add-to-list 'company-backends 'company-tern))
 
+(use-package json-mode
+  :defer-install t
+  :commands (json-mode json-mode-show-path json-mode-beautify)
+  :init
+
+  ;; Lazy-load json-mode. This requires some gymnastics. It concerns
+  ;; me somewhat that this kind of stuff now seems routine to me.
+
+  (el-patch-defconst json-mode-standard-file-ext '(".json" ".jsonld")
+    "List of JSON file extensions.")
+
+  (el-patch-defsubst json-mode--update-auto-mode (filenames)
+    "Update the `json-mode' entry of `auto-mode-alist'.
+
+FILENAMES should be a list of file as string.
+Return the new `auto-mode-alist' entry"
+    (let* ((new-regexp
+            (rx-to-string
+             `(seq (eval
+                    (cons 'or
+                          (append json-mode-standard-file-ext
+                                  ',filenames))) eot)))
+           (new-entry (cons new-regexp 'json-mode))
+           (old-entry (when (boundp 'json-mode--auto-mode-entry)
+                        json-mode--auto-mode-entry)))
+      (setq auto-mode-alist (delete old-entry auto-mode-alist))
+      (add-to-list 'auto-mode-alist new-entry)
+      new-entry))
+
+  (el-patch-defcustom json-mode-auto-mode-list '(".babelrc" ".bowerrc")
+    "List of filename as string to pass for the JSON entry of
+`auto-mode-alist'.
+
+Note however that custom `json-mode' entries in `auto-mode-alist'
+won’t be affected."
+    :group 'json-mode
+    :type '(repeat string)
+    :set (lambda (symbol value)
+           "Update SYMBOL with a new regexp made from VALUE.
+
+This function calls `json-mode--update-auto-mode' to change the
+`json-mode--auto-mode-entry' entry in `auto-mode-alist'."
+           (set-default symbol value)
+           (setq json-mode--auto-mode-entry (json-mode--update-auto-mode value))))
+
+  (el-patch-defvar json-mode--auto-mode-entry (json-mode--update-auto-mode json-mode-auto-mode-list)
+    "Regexp generated from the `json-mode-auto-mode-list'."))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Markdown
 
