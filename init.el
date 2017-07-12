713419a2e0ee4db8bcda02cd516e6893d0cc2c10
redpanda1234 <fkobayashi@g.hmc.edu>
2017-07-12 09:43:40 -0700

    ?

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defvar radian-minimum-emacs-version "25.1"
   "Radian Emacs does not support any Emacs version below this.")
 
