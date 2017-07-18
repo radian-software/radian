0d206020438e987e8e62ea1308e758a3ecce8c3b
Radon Rosborough <radon.neon@gmail.com>
2017-07-18 15:03:32 -0700

    Make ffap less eager

--- radian-emacs/radian-find-file.el
+++ radian-emacs/radian-find-file.el
@@ -421,6 +421,16 @@ This is a function for `after-save-hook'. Remove
   (with-eval-after-load 'projectile
     (counsel-projectile-toggle 1)))
 
+(use-package ffap
+  :ensure nil
+  :config
+
+  ;; Don't try to find URLs or remote machines.
+  (setq ffap-url-regexp nil)
+  (setq ffap-machine-p-local 'reject)
+  (setq ffap-machine-p-known 'reject)
+  (setq ffap-machine-p-unknown 'reject))
+
 (provide 'radian-find-file)
 
 ;;; radian-find-file.el ends here
