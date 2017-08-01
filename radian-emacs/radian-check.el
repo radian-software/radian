b77fafc9a877610c14f331fdcbf68adf81f3208a
Radon Rosborough <radon.neon@gmail.com>
2017-07-31 20:10:29 -0700

    Make flycheck-python-pycompile-executable safe

--- radian-emacs/radian-check.el
+++ radian-emacs/radian-check.el
@@ -8,6 +8,14 @@
   ;; Enable Flycheck everywhere unless otherwise specified.
   (global-flycheck-mode)
 
+  :config
+
+  ;; Make it safe to set the Python executable to some known-good
+  ;; values.
+  (dolist (name '("python" "python3"))
+    (add-to-list 'safe-local-variable-values
+                 `(flycheck-python-pycompile-executable . ,name)))
+
   :diminish flycheck-mode)
 
 (provide 'radian-check)
