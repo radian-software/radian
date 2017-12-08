f655630d09546bd755add12c67494095cd8ac7d8
Radon Rosborough <radon.neon@gmail.com>
2017-12-08 12:23:06 -0800

    [#389] Bind sp-join-sexp in sp-paredit-bindings

--- radian-emacs/radian-pairs.el
+++ radian-emacs/radian-pairs.el
@@ -15,9 +15,10 @@
   ;; Enable the functionality of Smartparens everywhere.
   (smartparens-global-mode +1)
 
-  ;; Smartparens' Paredit emulation is missing a binding for sexp
-  ;; convolution, so we re-add it here.
+  ;; Smartparens' Paredit emulation is missing some bindings, so we
+  ;; re-add them here.
   (radian-alist-set* "M-?" #'sp-convolute-sexp sp-paredit-bindings)
+  (radian-alist-set* "M-j" #'sp-join-sexp sp-paredit-bindings)
 
   ;; Enable some default keybindings for Smartparens.
   (sp-use-paredit-bindings)
