10576980f4a202fbb4e1d61696fcde69f1c9fc8a
Radon Rosborough <radon.neon@gmail.com>
2017-12-31 13:35:59 -0700

    Add argument injection to patch-macos-app.zsh

--- scripts/patch-macos-app.zsh
+++ scripts/patch-macos-app.zsh
@@ -12,7 +12,11 @@ function die {
 
 function usage {
     cat 1>&2 <<EOF
-usage: $script_name <app-name-or-path> <internal-binary-name> <backup-binary-name> (--patch | --unpatch)
+usage: $script_name <app-name-or-path>
+       [--binary <name>] [--alt-binary <name>]
+       [--prefix-args <arg>... --]
+       [--suffix-args <arg>... --]
+       [--patch | --unpatch | --repatch]
 
 This script patches a macOS application bundle (.app) so that
 ~/.profile is executed when the application launches. This allows you
@@ -24,17 +28,26 @@ The first argument is the path to a .app bundle, or just an
 application name, in which case the app is assumed to be in
 /Applications.
 
-The second argument is the name of the main binary in Contents/MacOS
-within the bundle. This script renames that binary to another name,
-given by the third argument, and then puts a shell script in its place
-that sources ~/.profile and then runs the original binary.
+--binary is the name of the main binary in Contents/MacOS within the
+bundle. This defaults to the name of the application without the
+extension. This script renames that binary to another name, given by
+--alt-binary (which defaults to the value of --binary plus the string
+'Core'), and then puts a shell script in its place that sources
+~/.profile and then runs the original binary.
 
-If --unpatch is given, then the binary is moved back to its original
-location and the wrapper script is deleted.
+You can provide --prefix-args and --suffix-args to inject some
+additional arguments into the argument list of the main binary, at the
+beginning and end respectively. Make sure to include '--' after the
+arguments to inject, since otherwise there is no way to tell where the
+list ends.
+
+If --unpatch is given (rather than --patch, the default), then the
+binary is moved back to its original location and the wrapper script
+is deleted. If --repatch is given it is equivalent to performing first
+--unpatch and then --patch.
 
 If you move the application, you must unpatch and repatch it using
-this script. Furthermore, note that the patch will not work if the
-binary being invoked has any single quotes in it.
+this script.
 EOF
     if (( $# == 1 )) && [[ $1 == *help ]]; then
         exit 0
@@ -43,7 +56,7 @@ EOF
     fi
 }
 
-if (( $# != 4 )); then
+if (( $# < 1 )); then
     usage
 fi
 
@@ -53,24 +66,83 @@ else
     app="/Applications/$1.app"
 fi
 
-binary=$2
+shift
+
+binary=
+alt_binary=
+typeset -a prefix_args=()
+typeset -a suffix_args=()
+patch=yes
+
+last=
+
+for arg; do
+    case $last in
+        "")
+            case $arg in
+                --prefix-args)
+                    prefix_args=()
+                    last=$arg
+                    ;;
+                --suffix-args)
+                    suffix_args=()
+                    last=$arg
+                    ;;
+                --patch)
+                    patch=yes
+                    ;;
+                --unpatch)
+                    patch=
+                    ;;
+                --repatch)
+                    patch=repatch
+                    ;;
+                *)
+                    last=$arg
+                    ;;
+            esac
+            ;;
+        --binary)
+            binary=$arg
+            last=
+            ;;
+        --alt-binary)
+            alt_binary=$arg
+            last=
+            ;;
+        --prefix-args)
+            if [[ $arg == -- ]]; then
+                last=
+            else
+                prefix_args+=($arg)
+            fi
+            ;;
+        --suffix-args)
+            if [[ $arg == -- ]]; then
+                last=
+            else
+                suffix_args+=($arg)
+            fi
+            ;;
+        *)
+            die "unknown argument: $last"
+            ;;
+    esac
+done
+
+if [[ -n $last ]]; then
+    die "needs more arguments: $last"
+fi
+
+binary=${binary:-${${app##*/}%.app}}
+alt_binary=${alt_binary:-${binary}Core}
 
 if [[ $binary == */* ]]; then
     die "cannot contain slashes: $binary"
 fi
 
-backup=$3
-
-if [[ $backup == */* ]]; then
-    die "cannot contain slashes: $backup"
-fi
-
-if [[ $4 == --patch ]]; then
-    patch=yes
-elif [[ $4 == --unpatch ]]; then
-    patch=
-else
-    usage
+if [[ $alt_binary == */* ]]; then
+    die "cannot contain slashes: $alt_binary"
 fi
 
 if [[ ! -d $app ]]; then
@@ -79,34 +151,56 @@ fi
 
 cd $app/Contents/MacOS
 
-if [[ -n $patch ]]; then
-    if [[ ! -x $binary ]]; then
-        die "no such executable: $PWD/$binary"
+function main {
+    if [[ -n $patch ]]; then
+        if [[ ! -x $binary ]]; then
+            die "no such executable: $PWD/$binary"
+        fi
+
+        if [[ -e $alt_binary || -L $alt_binary ]]; then
+            die "already exists: $PWD/$alt_binary"
+        fi
+    else
+        if [[ ! -x $alt_binary ]]; then
+            die "no such executable: $PWD/$alt_binary"
+        fi
     fi
 
-    if [[ -e $backup || -L $backup ]]; then
-        die "already exists: $PWD/$backup"
-    fi
-else
-    if [[ ! -x $backup ]]; then
-        die "no such executable: $PWD/$backup"
-    fi
-fi
-
-if [[ -n $patch ]]; then
-    mv $binary $backup
-    cat >$binary <<EOF
+    if [[ -n $patch ]]; then
+        mv $binary $alt_binary
+        local abs_path="$PWD/$alt_binary"
+        if (( ${#prefix_args[@]} > 0 )); then
+            local prefix=" ${(@q)prefix_args}"
+        else
+            local prefix=
+        fi
+        if (( ${#suffix_args[@]} > 0 )); then
+            local suffix=" ${(@q)suffix_args}"
+        else
+            local suffix=
+        fi
+        cat >$binary <<EOF
 #!/bin/sh
 
 if [ -f ~/.profile ]; then
     . ~/.profile
 fi
 
-'$PWD/$backup' "\$@"
+${(q)abs_path}${prefix} "\$@"${suffix}
 EOF
-    chmod +x $binary
-    echo "Patched successfully: $app"
+        chmod +x $binary
+        echo "Patched successfully: $app"
+    else
+        mv $alt_binary $binary
+        echo "Unpatched successfully: $app"
+    fi
+}
+
+if [[ $patch == repatch ]]; then
+    patch=
+    main
+    patch=yes
+    main
 else
-    mv $backup $binary
-    echo "Unpatched successfully: $app"
+    main
 fi
