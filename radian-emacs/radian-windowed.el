70bd258bcef191553207bbc92ba68190b215532f
Radon Rosborough <radon.neon@gmail.com>
2018-06-03 11:44:47 -0600

    Remove old header

--- radian-emacs/radian-windowed.el
+++ radian-emacs/radian-windowed.el
@@ -1,8 +1,5 @@
 ;;; radian-windowed.el --- Checking the window system
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; Utility functions for checking the window system
-
 (defmacro radian-with-windowed-emacs (&rest body)
   "Eval BODY if Emacs is windowed, else return nil."
   (declare (indent defun))
