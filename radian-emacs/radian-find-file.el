207fefe3e37d65994fde33ef110ea9ac2595728b
Radon Rosborough <radon.neon@gmail.com>
2018-06-06 12:08:31 -0600

    Update counsel-projectile and config

--- radian-emacs/radian-find-file.el
+++ radian-emacs/radian-find-file.el
@@ -375,12 +375,6 @@ This is a function for `after-save-hook'. Remove
 ;; This package provides enhanced versions of the Projectile commands
 ;; that use Ivy.
 (use-package counsel-projectile
-  ;; My fork remaps the 'f' action to do a find-file rather than just
-  ;; the same as pressing M-o again.
-  :straight (:host github
-                   :repo "raxod502/counsel-projectile"
-                   :upstream (:host github
-                                    :repo "ericdanan/counsel-projectile"))
   :init
 
   ;; Lazy-load `counsel-projectile'.
