a82e22336bdd58cc2246fe04c337f75499a0b5c5
Radon Rosborough <radon.neon@gmail.com>
2018-09-22 10:05:25 -0700

    Disable dir auto-creation for remote files
    It was totally broken and proper support requires significantly more
    effort, so let's disable it for now.
    
    See https://github.com/raxod502/radian/issues/434.

--- emacs/radian.el
+++ emacs/radian.el
@@ -1151,84 +1151,91 @@ the file being visited, if necessary. It also sets a buffer-local
 variable so that the user will be prompted to delete the newly
 created directories if they kill the buffer without saving it.
 
+This advice has no effect for remote files.
+
 This is an `:around' advice for `find-file' and similar
 functions."
-  (let ((orig-filename filename)
-        ;; For relative paths where none of the named parent
-        ;; directories exist, we might get a nil from
-        ;; `file-name-directory' below, which would be bad. Thus we
-        ;; expand the path fully.
-        (filename (expand-file-name filename))
-        ;; The variable `dirs-to-delete' is a list of the directories
-        ;; that will be automatically created by `make-directory'. We
-        ;; will want to offer to delete these directories if the user
-        ;; kills the buffer without saving it.
-        (dirs-to-delete ()))
-    ;; If the file already exists, we don't need to worry about
-    ;; creating any directories.
-    (unless (file-exists-p filename)
-      ;; It's easy to figure out how to invoke `make-directory',
-      ;; because it will automatically create all parent directories.
-      ;; We just need to ask for the directory immediately containing
-      ;; the file to be created.
-      (let* ((dir-to-create (file-name-directory filename))
-             ;; However, to find the exact set of directories that
-             ;; might need to be deleted afterward, we need to iterate
-             ;; upward through the directory tree until we find a
-             ;; directory that already exists, starting at the
-             ;; directory containing the new file.
-             (current-dir dir-to-create))
-        ;; If the directory containing the new file already exists,
-        ;; nothing needs to be created, and therefore nothing needs to
-        ;; be destroyed, either.
-        (while (not (file-exists-p current-dir))
-          ;; Otherwise, we'll add that directory onto the list of
-          ;; directories that are going to be created.
-          (push current-dir dirs-to-delete)
-          ;; Now we iterate upwards one directory. The
-          ;; `directory-file-name' function removes the trailing slash
-          ;; of the current directory, so that it is viewed as a file,
-          ;; and then the `file-name-directory' function returns the
-          ;; directory component in that path (which means the parent
-          ;; directory).
-          (setq current-dir (file-name-directory
-                             (directory-file-name current-dir))))
-        ;; Only bother trying to create a directory if one does not
-        ;; already exist.
-        (unless (file-exists-p dir-to-create)
-          ;; Make the necessary directory and its parents.
-          (make-directory dir-to-create 'parents))))
-    ;; Call the original `find-file', now that the directory
-    ;; containing the file to found exists. We make sure to preserve
-    ;; the return value, so as not to mess up any commands relying on
-    ;; it.
-    (prog1 (apply find-file orig-filename args)
-      ;; If there are directories we want to offer to delete later, we
-      ;; have more to do.
-      (when dirs-to-delete
-        ;; Since we already called `find-file', we're now in the buffer
-        ;; for the new file. That means we can transfer the list of
-        ;; directories to possibly delete later into a buffer-local
-        ;; variable. But we pushed new entries onto the beginning of
-        ;; `dirs-to-delete', so now we have to reverse it (in order to
-        ;; later offer to delete directories from innermost to
-        ;; outermost).
-        (setq-local radian--dirs-to-delete (reverse dirs-to-delete))
-        ;; Now we add a buffer-local hook to offer to delete those
-        ;; directories when the buffer is killed, but only if it's
-        ;; appropriate to do so (for instance, only if the directories
-        ;; still exist and the file still doesn't exist).
-        (add-hook 'kill-buffer-hook
-                  #'radian--kill-buffer-delete-directory-if-appropriate
-                  'append 'local)
-        ;; The above hook removes itself when it is run, but that will
-        ;; only happen when the buffer is killed (which might never
-        ;; happen). Just for cleanliness, we automatically remove it
-        ;; when the buffer is saved. This hook also removes itself when
-        ;; run, in addition to removing the above hook.
-        (add-hook 'after-save-hook
-                  #'radian--remove-kill-buffer-delete-directory-hook
-                  'append 'local)))))
+  (if (file-remote-p filename)
+      (apply find-file filename args)
+    (let ((orig-filename filename)
+          ;; For relative paths where none of the named parent
+          ;; directories exist, we might get a nil from
+          ;; `file-name-directory' below, which would be bad. Thus we
+          ;; expand the path fully.
+          (filename (expand-file-name filename))
+          ;; The variable `dirs-to-delete' is a list of the
+          ;; directories that will be automatically created by
+          ;; `make-directory'. We will want to offer to delete these
+          ;; directories if the user kills the buffer without saving
+          ;; it.
+          (dirs-to-delete ()))
+      ;; If the file already exists, we don't need to worry about
+      ;; creating any directories.
+      (unless (file-exists-p filename)
+        ;; It's easy to figure out how to invoke `make-directory',
+        ;; because it will automatically create all parent
+        ;; directories. We just need to ask for the directory
+        ;; immediately containing the file to be created.
+        (let* ((dir-to-create (file-name-directory filename))
+               ;; However, to find the exact set of directories that
+               ;; might need to be deleted afterward, we need to
+               ;; iterate upward through the directory tree until we
+               ;; find a directory that already exists, starting at
+               ;; the directory containing the new file.
+               (current-dir dir-to-create))
+          ;; If the directory containing the new file already exists,
+          ;; nothing needs to be created, and therefore nothing needs
+          ;; to be destroyed, either.
+          (while (not (file-exists-p current-dir))
+            ;; Otherwise, we'll add that directory onto the list of
+            ;; directories that are going to be created.
+            (push current-dir dirs-to-delete)
+            ;; Now we iterate upwards one directory. The
+            ;; `directory-file-name' function removes the trailing
+            ;; slash of the current directory, so that it is viewed as
+            ;; a file, and then the `file-name-directory' function
+            ;; returns the directory component in that path (which
+            ;; means the parent directory).
+            (setq current-dir (file-name-directory
+                               (directory-file-name current-dir))))
+          ;; Only bother trying to create a directory if one does not
+          ;; already exist.
+          (unless (file-exists-p dir-to-create)
+            ;; Make the necessary directory and its parents.
+            (make-directory dir-to-create 'parents))))
+      ;; Call the original `find-file', now that the directory
+      ;; containing the file to found exists. We make sure to preserve
+      ;; the return value, so as not to mess up any commands relying
+      ;; on it.
+      (prog1 (apply find-file orig-filename args)
+        ;; If there are directories we want to offer to delete later,
+        ;; we have more to do.
+        (when dirs-to-delete
+          ;; Since we already called `find-file', we're now in the
+          ;; buffer for the new file. That means we can transfer the
+          ;; list of directories to possibly delete later into a
+          ;; buffer-local variable. But we pushed new entries onto the
+          ;; beginning of `dirs-to-delete', so now we have to reverse
+          ;; it (in order to later offer to delete directories from
+          ;; innermost to outermost).
+          (setq-local radian--dirs-to-delete (reverse dirs-to-delete))
+          ;; Now we add a buffer-local hook to offer to delete those
+          ;; directories when the buffer is killed, but only if it's
+          ;; appropriate to do so (for instance, only if the
+          ;; directories still exist and the file still doesn't
+          ;; exist).
+          (add-hook 'kill-buffer-hook
+                    #'radian--kill-buffer-delete-directory-if-appropriate
+                    'append 'local)
+          ;; The above hook removes itself when it is run, but that
+          ;; will only happen when the buffer is killed (which might
+          ;; never happen). Just for cleanliness, we automatically
+          ;; remove it when the buffer is saved. This hook also
+          ;; removes itself when run, in addition to removing the
+          ;; above hook.
+          (add-hook 'after-save-hook
+                    #'radian--remove-kill-buffer-delete-directory-hook
+                    'append 'local))))))
 
 (defun radian--kill-buffer-delete-directory-if-appropriate ()
   "Delete parent directories if appropriate.
