9d66e8794de3ac3670cc12736204f67aaf950c2c
Radon Rosborough <radon.neon@gmail.com>
2019-08-14 10:57:19 -0700

    Improve byte-compile error messages

--- scripts/byte-compile.bash
+++ scripts/byte-compile.bash
@@ -8,5 +8,6 @@ set -o pipefail
        --load "$HOME/.emacs.d/init.el"                       \
        --funcall radian-byte-compile 2>&1                    \
      | (grep -v "In toplevel form"                  || true) \
+     | (grep -v "In end of data"                    || true) \
      | (grep -v "Warning: Package cl is deprecated" || true) \
      | (! grep .)) || (rm -f emacs/radian.elc; false)
