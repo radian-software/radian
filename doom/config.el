26e0b8717199cfc24338de0875c2369245ed1d46
Radon Rosborough <radon.neon@gmail.com>
2020-06-16 10:24:38 -0600

    Bind describe-keymap

--- doom/config.el
+++ doom/config.el
@@ -1,8 +1,10 @@
 ;; -*- lexical-binding: t -*-
 
+(map! :leader
+      :desc "describe-keymap" "hK" #'describe-keymap) h
+
 (setq doom-theme 'doom-vibrant)
 (setq display-line-numbers-type 'relative)
-
 (add-hook 'window-setup-hook #'toggle-frame-maximized)
 
 (load (expand-file-name "config.local.el") 'noerror 'nomessage)
