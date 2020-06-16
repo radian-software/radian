330e037397ee4a016f0a2dbe6bb287591048a48a
Radon Rosborough <radon.neon@gmail.com>
2020-06-16 09:45:58 -0600

    More vibrant color theme, maximize on startup

--- doom/config.el
+++ doom/config.el
@@ -1,5 +1,8 @@
 ;; -*- lexical-binding: t -*-
 
+(setq doom-theme 'doom-vibrant)
 (setq display-line-numbers-type 'relative)
+
+(add-hook 'window-setup-hook #'toggle-frame-maximized)
 
 (load (expand-file-name "config.local.el") 'noerror 'nomessage)
