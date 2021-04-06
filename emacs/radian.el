403277c4772a314edf3feef1815d17e05d8df843
Al Haji-Ali <a.hajiali@hw.ac.uk>
2021-04-06 14:57:14 +0100

    Replace radian-new-emacs with restart-emacs-start-new-emacs

--- emacs/radian.el
+++ emacs/radian.el
@@ -4994,9 +4994,7 @@ spam. This advice, however, inhibits the message for everyone.")
 ;; Package `restart-emacs' provides an easy way to restart Emacs from
 ;; inside of Emacs, both in the terminal and in windowed mode.
 (use-package restart-emacs
-  :commands (radian-new-emacs)
   :init
-
   (defvar radian--restart-in-progress nil
     "Used to prevent infinite recursion.
 This is non-nil if `radian--advice-kill-emacs-dispatch' has called
@@ -5074,66 +5072,12 @@ bound dynamically before being used.")
                 ((or ?r ?R)
                  (restart-emacs arg))
                 ((or ?e ?E)
-                 (radian-new-emacs
+                 (restart-emacs-start-new-emacs
                   (restart-emacs--translate-prefix-to-args arg)))
                 ((or ?k ?K) (radian-really-kill-emacs))
                 (?\C-g (signal 'quit nil))
                 (_ (setq key nil))))))
-        (message "%s%c" prompt key))))
-
-  :config/el-patch
-
-  (defun (el-patch-swap restart-emacs radian-new-emacs)
-      (&optional args)
-    (el-patch-concat
-      (el-patch-swap
-        "Restart Emacs."
-        "Start a new Emacs session without killing the current one.")
-      "
-
-When called interactively ARGS is interpreted as follows
-
-- with a single `universal-argument' (`C-u') Emacs is "
-      (el-patch-swap "restarted" "started")
-      "
-  with `--debug-init' flag
-- with two `universal-argument' (`C-u') Emacs is "
-      (el-patch-swap "restarted" "started")
-      " with
-  `-Q' flag
-- with three `universal-argument' (`C-u') the user prompted for
-  the arguments
-
-When called non-interactively ARGS should be a list of arguments
-with which Emacs should be "
-      (el-patch-swap "restarted" "started")
-      ".")
-    (interactive "P")
-    ;; Do not trigger a restart unless we are sure, we can restart emacs
-    (restart-emacs--ensure-can-restart)
-    ;; We need the new emacs to be spawned after all kill-emacs-hooks
-    ;; have been processed and there is nothing interesting left
-    (let* ((default-directory (restart-emacs--guess-startup-directory))
-           (translated-args (if (called-interactively-p 'any)
-                                (restart-emacs--translate-prefix-to-args args)
-                              args))
-           (restart-args (append translated-args
-                                 ;; When Emacs is started with a -Q
-                                 ;; restart-emacs's autoloads would
-                                 ;; not be present causing the the
-                                 ;; --restart-emacs-desktop argument
-                                 ;; to be unhandled
-                                 (unless (member "-Q" translated-args)
-                                   (restart-emacs--frame-restore-args))))
-           (el-patch-remove
-             (kill-emacs-hook
-              (append kill-emacs-hook
-                      (list (apply-partially
-                             #'restart-emacs--launch-other-emacs
-                             restart-args))))))
-      (el-patch-swap
-        (save-buffers-kill-emacs)
-        (restart-emacs--launch-other-emacs restart-args)))))
+        (message "%s%c" prompt key)))))
 
 ;;; Miscellaneous
 
