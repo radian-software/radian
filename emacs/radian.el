bb039d3d91688cb21417e6a44a0a7f8b86ae02a9
Radon Rosborough <radon.neon@gmail.com>
2021-12-26 12:51:01 -0800

    Allow disabling delsel

--- emacs/radian.el
+++ emacs/radian.el
@@ -1037,7 +1037,12 @@ active minibuffer, even if the minibuffer is not selected."
   (if-let ((minibuffer (active-minibuffer-window)))
       (progn
         (switch-to-buffer (window-buffer minibuffer))
-        (minibuffer-keyboard-quit))
+        (if (featurep 'delsel)
+            (progn
+              (eval-when-compile
+                (require 'delsel))
+              (minibuffer-keyboard-quit))
+          (abort-minibuffers)))
     (funcall keyboard-quit)))
 
 (radian-defadvice radian--advice-kill-buffer-maybe-kill-window
