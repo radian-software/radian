20796e554bd090f2c023ce40338d536cce7bf179
Radon Rosborough <radon@intuitiveexplanations.com>
2023-02-27 20:19:24 -0800

    Remove deprecated options
    These have been deprecated for >1 year. Remove.

--- emacs/radian.el
+++ emacs/radian.el
@@ -49,26 +49,6 @@
 
 ;;; Define utility functions and variables
 
-(defcustom radian-org-enable-contrib nil
-  "Non-nil means to make Org contrib modules available.
-This has to be set at the beginning of init, i.e. in the top
-level of init.local.el."
-  :type 'boolean)
-
-(defcustom radian-color-theme-enable t
-  "Non-nil means to load the default Radian color theme.
-Set this to nil if you wish to load a different color theme in
-your local configuration."
-  :type 'boolean)
-
-(make-obsolete-variable 'radian-org-enable-contrib
-                        'radian-disabled-packages
-                        "2021-11-28")
-
-(make-obsolete-variable 'radian-color-theme-enable
-                        'radian-disabled-packages
-                        "2021-11-28")
-
 (defvar radian-disabled-packages nil
   "List of packages that Radian should not load.
 
@@ -78,14 +58,6 @@ this list.")
 
 (defvar radian-compiling nil
   "Non-nil when Radian's make is being called.")
-
-(unless radian-org-enable-contrib
-  (add-to-list 'radian-disabled-packages
-               'org-contrib))
-
-(unless radian-color-theme-enable
-  (add-to-list 'radian-disabled-packages
-               'zerodark-theme))
 
 (defvar radian-directory (file-name-directory
                           (directory-file-name
