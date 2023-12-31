ec4615b3a59d66927cc72d3efdd3e7bfa97512ac
Radon Rosborough <radon@intuitiveexplanations.com>
2023-12-31 10:14:35 -0700

    Fix CI for Emacs 27

--- scripts/radian-indent.el
+++ scripts/radian-indent.el
@@ -3,8 +3,15 @@
 ;; ensure that various things are indented correctly if they require
 ;; some setup for Emacs to know how to do the right thing.
 
+;; Load libraries whose definitions we need to overwrite.
+(require 'subr-x)
+
 ;; The indentation of `define-key' has for some reason changed in
 ;; Emacs 29 when it was deprecated in favor of `keymap-set'. Maybe
 ;; that is a bug and they will change it, but for now, force the
 ;; indentation to the backwards-compatible version.
 (put #'define-key 'lisp-indent-function 'defun)
+
+;; The indentation of `thread-first' changed from (indent 1) to
+;; (indent 0) in Emacs 28. Use the later version.
+(put #'thread-first 'lisp-indent-function 0)
