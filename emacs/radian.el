9378c24df9b657a396acf7ef60759ccc18ee6a3a
Radon Rosborough <radon@intuitiveexplanations.com>
2026-05-28 15:32:44 -0700

    Update out-of-date el-patches

--- emacs/radian.el
+++ emacs/radian.el
@@ -3504,8 +3504,8 @@ It hangs the editor because it wants to make remote process calls."
                  map (char-to-string closing) 'ruby-electric-closing-char)))))
       map)
     (el-patch-concat
-      "Keymap used in ruby-electric-mode"
-      (el-patch-add ".\n\nThe single-character bindings have been removed.")))
+      "Keymap used in ruby-electric-mode."
+      (el-patch-add "\n\nThe single-character bindings have been removed.")))
 
   :init
 
@@ -3819,52 +3819,6 @@ Workaround for <https://github.com/fxbois/web-mode/issues/1263>."
 
 ;; Package `json-mode' provides a major mode for JSON.
 (radian-use-package json-mode
-  :init/el-patch
-
-  (defconst json-mode-standard-file-ext '(".json" ".jsonld")
-    "List of JSON file extensions.")
-
-  (defsubst json-mode--update-auto-mode (filenames)
-    "Update the `json-mode' entry of `auto-mode-alist'.
-
-FILENAMES should be a list of file as string.
-Return the new `auto-mode-alist' entry"
-    (let* ((new-regexp
-            (rx-to-string
-             `(seq (eval
-                    (cons 'or
-                          (append json-mode-standard-file-ext
-                                  ',filenames)))
-                   eot)))
-           (new-entry (cons new-regexp 'json-mode))
-           (old-entry (when (boundp 'json-mode--auto-mode-entry)
-                        json-mode--auto-mode-entry)))
-      (setq auto-mode-alist (delete old-entry auto-mode-alist))
-      (add-to-list 'auto-mode-alist new-entry)
-      new-entry))
-
-  (defcustom json-mode-auto-mode-list '(".babelrc"
-                                        ".bowerrc"
-                                        "composer.lock")
-    "List of filenames for the JSON entry of `auto-mode-alist'.
-
-Note however that custom `json-mode' entries in `auto-mode-alist'
-won’t be affected."
-    :group 'json
-    :type '(repeat string)
-    :set (lambda (symbol value)
-           "Update SYMBOL with a new regexp made from VALUE.
-
-This function calls `json-mode--update-auto-mode' to change the
-`json-mode--auto-mode-entry' entry in `auto-mode-alist'."
-           (set-default symbol value)
-           (setq json-mode--auto-mode-entry
-                 (json-mode--update-auto-mode value))))
-
-  (defvar json-mode--auto-mode-entry
-    (json-mode--update-auto-mode json-mode-auto-mode-list)
-    "Regexp generated from the `json-mode-auto-mode-list'.")
-
   :config
 
   (radian-defhook radian--fix-json-indentation ()
